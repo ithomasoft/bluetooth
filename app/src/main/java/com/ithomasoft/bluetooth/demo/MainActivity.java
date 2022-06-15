54d4d20a8c7a0ef5ae8e0ef309812a3f5ebd5956
iThomas <953530507@qq.com>
2022-06-15 22:16:16 +0800

    完善设备连接监听

--- app/src/main/java/com/ithomasoft/bluetooth/demo/MainActivity.java
+++ app/src/main/java/com/ithomasoft/bluetooth/demo/MainActivity.java
@@ -2,8 +2,14 @@ package com.ithomasoft.bluetooth.demo;
 
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,6 +22,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.listener.OnItemLongClickListener;
+import com.chad.library.adapter.base.viewholder.BaseViewHolder;
+import com.hjq.permissions.OnPermissionCallback;
+import com.hjq.permissions.Permission;
+import com.hjq.permissions.XXPermissions;
 import com.ithomasoft.bluetooth.Bluetooth;
 import com.ithomasoft.bluetooth.listener.BluetoothConnectListener;
 import com.ithomasoft.bluetooth.listener.BluetoothPairListener;
@@ -24,17 +34,25 @@ import com.ithomasoft.bluetooth.listener.DiscoveryDevicesListener;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
     private Button btnOpen;
     private Button btnClose;
     private Button btnStart;
     private Button btnStop;
     private TextView tvState;
-    private RecyclerView rvDevice;
+    private RecyclerView rvDevice, rvLocation;
     private DeviceAdapter mAdapter;
     private DeviceInfo pairedTitle = new DeviceInfo("已配对设备", false);
     private DeviceInfo unpairedTitle = new DeviceInfo("可用设备", false);
+    private BaseQuickAdapter<String, BaseViewHolder> locationAdapter = new BaseQuickAdapter<String, BaseViewHolder>(R.layout.item_location) {
+        @Override
+        protected void convert(BaseViewHolder baseViewHolder, String s) {
+            baseViewHolder.setText(R.id.tv_title, s);
+        }
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,9 +65,12 @@ public class MainActivity extends AppCompatActivity {
         btnStop = findViewById(R.id.btn_stop);
         tvState = findViewById(R.id.tv_state);
         rvDevice = findViewById(R.id.rv_device);
+        rvLocation = findViewById(R.id.rv_location);
         mAdapter = new DeviceAdapter();
         rvDevice.setLayoutManager(new LinearLayoutManager(this));
         rvDevice.setAdapter(mAdapter);
+        rvLocation.setLayoutManager(new LinearLayoutManager(this));
+        rvLocation.setAdapter(locationAdapter);
 
         Bluetooth.getInstance().setBlueStateListener(new BluetoothStateListener() {
 
@@ -110,6 +131,7 @@ public class MainActivity extends AppCompatActivity {
             public void onBTDeviceConnected(String address, String name) {
                 mAdapter.notifyDataSetChanged();
                 Toast.makeText(MainActivity.this, "连接成功", Toast.LENGTH_SHORT).show();
+                registerSouthGnss();
             }
 
             @Override
@@ -161,6 +183,7 @@ public class MainActivity extends AppCompatActivity {
                         mAdapter.notifyItemChanged(mAdapter.getItemPosition(unpairedTitle));
                     }
 
+                    @SuppressLint("MissingPermission")
                     @Override
                     public void discoveryNew(BluetoothDevice device) {
                         if (device != null) {
@@ -211,6 +234,22 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private LocationManager mLocationManager;
+
+    @SuppressLint("MissingPermission")
+    private void registerSouthGnss() {
+        XXPermissions.with(this).permission(Permission.ACCESS_FINE_LOCATION, Permission.ACCESS_COARSE_LOCATION).request(new OnPermissionCallback() {
+            @Override
+            public void onGranted(List<String> permissions, boolean all) {
+                mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//                mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+//                        1000, 0, MainActivity.this);
+                mLocationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, MainActivity.this, Looper.getMainLooper());
+            }
+        });
+
+    }
+
     @SuppressLint("MissingPermission")
     private void unPairDevice(BluetoothDevice bluetoothDevice) {
         if (bluetoothDevice.getBondState() == BluetoothDevice.BOND_BONDED) {
@@ -218,6 +257,7 @@ public class MainActivity extends AppCompatActivity {
             Bluetooth.getInstance().disconnect();
         }
     }
+
     @SuppressLint("MissingPermission")
     private void pairDevice(BluetoothDevice bluetoothDevice) {
         Bluetooth.getInstance().cancelDiscovery();
@@ -260,5 +300,47 @@ public class MainActivity extends AppCompatActivity {
             btnStart.setEnabled(false);
             btnStop.setEnabled(false);
         }
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        Log.e("location", location.toString());
+        locationAdapter.addData("onLocationChanged:" + location.toString());
+        if (location != null) {
+            onStatusChanged(LocationManager.GPS_PROVIDER, 111, location.getExtras());
+        }
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull List<Location> locations) {
+//        Log.e("location", location.toString());
+    }
+
+    @Override
+    public void onFlushComplete(int requestCode) {
+//        Log.e("location", location.toString());
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.e("location", "onStatusChanged:" + provider + "--" + status + "--");
+        locationAdapter.addData("onStatusChanged:" + provider + "--" + status + "--");
+        Set<String> set = extras.keySet();
+        for (String key : set) {
+            locationAdapter.addData("onStatusChanged:" + key + "--" + extras.get(key));
+        }
+
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+        Log.e("location", "onProviderEnabled:" + provider);
+        locationAdapter.addData("onProviderEnabled:" + provider);
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+        Log.e("location", "onProviderDisabled:" + provider);
+        locationAdapter.addData("onProviderDisabled:" + provider);
     }
 }
